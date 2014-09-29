17c10a3bb10774afbf7cded9059e8e799758158c
bhavani <bhavani.gdm@gmail.com>
2014-09-28 22:39:48 -0700

    css styles for dash board

--- spec/views/tasks/edit.html.haml_spec.rb
+++ spec/views/tasks/edit.html.haml_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe "tasks/edit", :type => :view do
     assert_select "form[action=?][method=?]", task_path(@task), "post" do
 
       assert_select "input#task_name[name=?]", "task[name]"
-
-      assert_select "input#task_user_id[name=?]", "task[user_id]"
     end
   end
 end
--- spec/views/tasks/new.html.haml_spec.rb
+++ spec/views/tasks/new.html.haml_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe "tasks/new", :type => :view do
     assert_select "form[action=?][method=?]", tasks_path, "post" do
 
       assert_select "input#task_name[name=?]", "task[name]"
-
-      assert_select "input#task_user_id[name=?]", "task[user_id]"
     end
   end
 end
