4c0f779de2525b3beb81cf24f27159763062d5c5
bhavani <bhavani.gdm@gmail.com>
2014-09-29 17:00:10 -0700

    model specs for task and user models

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it "should have name"   do
+      task = Task.new(name: "" )
+      task.save
+      expect(task.errors[:name]).to include("can't be blank")
+    end
+    it "should have description"   do
+      task = Task.new(description: "" )
+      task.save
+      expect(task.errors[:description]).to include("can't be blank")
+    end
+  end
+  context 'associations' do
+    it "should belong to user" do
+      t = Task.new(name: 'spec_task',description: 'write model spec')
+      u = User.new(name: 'george')
+      t.user = u
+      t.save
+      expect(t.user).to eq u
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+  RSpec.describe Task, :type => :model do
+    context 'validations' do
+      it "should have name"   do
+        user = User.new(name: "" )
+        user.save
+        expect(user.errors[:name]).to include("can't be blank")
+      end
+    end
+    context 'associations' do
+      it "has many tasks" do
+        t1 = Task.new(name: 'spec_task',description: 'write model spec')
+        t2 = Task.new(name: 'model_spec_task',description: 'write user model spec')
+        u = User.new(name: 'george')
+        t1.user = u
+        t2.user = u
+        t1.save
+        t2.save
+        expect(u.tasks).to eq [t1,t2]
+      end
+    end
+  end
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
