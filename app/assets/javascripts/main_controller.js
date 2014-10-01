bac4603b109b4a98bb5348f821eba63151ab28d8
bhavani <bhavani.gdm@gmail.com>
2014-10-01 00:19:33 -0700

    angular js href issues fixed with ng clicks :-(

--- app/assets/javascripts/main_controller.js
+++ app/assets/javascripts/main_controller.js
@@ -1,6 +1,7 @@
 angular.module('taskApp.controllers.mainController', [])
   .controller('MainController', ['$scope', '$location' ,function($scope, $location){
     $scope.goto = function(path){
+//      $location.path = path;
       window.location.href = path;
     }
   }]
